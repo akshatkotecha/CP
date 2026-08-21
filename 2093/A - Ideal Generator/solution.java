d0e48a02658f89bfecfb3e6b665a847c4ab0ce67
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:16:27 +0530

    Add Ideal Generator [A] from Codeforces

---
+++ 2093/A - Ideal Generator/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0)
+        {
+            int n=sc.nextInt();
+            if(n%2==1) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
