5e6b15966cc1556a92eab6fd7b33ac9e7af20630
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:28:08 +0530

    Add Make it Beautiful [A] from Codeforces

---
+++ 1783/A - Make it Beautiful/solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			int a[]=new int[n];
+			for(int i=0;i<n;i++) a[i]=sc.nextInt();
+			Arrays.sort(a);
+			if(a[0]==a[n-1]) System.out.println("NO");
+			else {
+			    System.out.println("YES");
+			    System.out.print(a[n-1]+" ");
+			    for(int i=0;i<n-1;i++) System.out.print(a[i]+" ");
+			    System.out.println();
+			}
+		}
+	}
+}
+ 
+ 
