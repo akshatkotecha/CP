9879851d0b70e1d9a6caa2dd81ebbd61eda022cd
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:11:21 +0530

    Add Binary Array Game [A] from Codeforces

---
+++ 2183/A - Binary Array Game/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner sc=new Scanner(System.in);
+	   int t=sc.nextInt();
+	   while(t-->0)
+	   {
+	       int n=sc.nextInt();
+	       int a[]=new int[n];
+	       for(int i=0;i<n;i++) a[i]=sc.nextInt();
+	       if(a[0]+a[n-1]==0) System.out.println("Bob");
+	       else System.out.println("Alice");
+	   }
+ 
+	}
+}
