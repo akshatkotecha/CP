c6273c2bac3688c2b63dedded22d4d537df65fa2
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:35:13 +0530

    Add Divide and Conquer [A] from Codeforces

---
+++ 2241/A - Divide and Conquer/solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int x=sc.nextInt();
+			int y=sc.nextInt();
+			if(x>=y && x%y==0) System.out.println("YES");
+			else System.out.println("NO");
+		}
+	}
+}
+ 
+ 
