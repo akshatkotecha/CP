43baadbbd16b90c0d8dd3c2efd3f7622eee40f2c
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:09:22 +0530

    Add Blackslex and Password [A] from Codeforces

---
+++ 2179/A - Blackslex and Password/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner sc=new Scanner(System.in);
+	   int t=sc.nextInt();
+	   while(t-->0)
+	   {
+	       int k=sc.nextInt();
+	       int x=sc.nextInt();
+	       int f=k*x+1;
+	       System.out.println(f);
+	   }
+ 
+	}
+}
