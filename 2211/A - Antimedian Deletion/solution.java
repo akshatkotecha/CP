4c85885bbcec98622b3865ab6bbaff1ef07ca14f
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:19:23 +0530

    Add Antimedian Deletion [A] from Codeforces

---
+++ 2211/A - Antimedian Deletion/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			int a[]=new int[n];
+			for(int i=0;i<n;i++) a[i]=sc.nextInt();
+			if(n==1) {System.out.println(1); continue;}
+			for(int i=0;i<n;i++)
+			{
+			   System.out.print(2+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+ 
