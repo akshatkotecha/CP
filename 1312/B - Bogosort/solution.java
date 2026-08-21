adbdd76d6753bbdfaad22fdc25aa7e0ff5b7e26a
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:26:40 +0530

    Add Bogosort [B] from Codeforces

---
+++ 1312/B - Bogosort/solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			int a[]=new int[n];
+			for(int i=0;i<n;i++) a[i]=sc.nextInt();
+			Arrays.sort(a);
+			for(int i=n-1;i>=0;i--) System.out.print(a[i]+" ");
+			System.out.println();
+		}
+	}
+}
+ 
+ 
