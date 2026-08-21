39535fdcc12a3765c91c1e7105f8611076fcf431
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:36:13 +0530

    Add Stepan and Permutation [C] from Codeforces

---
+++ 2244/C - Stepan and Permutation/solution.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			int x=sc.nextInt();
+			int y=sc.nextInt();
+			int a[]=new int[n];
+	        for(int i=0;i<n;i++) a[i]=sc.nextInt();
+	        int b=gcd(x,y);
+	        boolean ok=true;
+	        for(int i=0;i<n;i++){
+	            if(((i+1)-a[i])%b!=0){
+	                ok=false;
+	                break;
+	            }
+	        }
+	        if(ok) System.out.println("YES");
+	        else System.out.println("NO");
+		}
+	}
+}
+ 
+ 
