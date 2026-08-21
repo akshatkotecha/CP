28771b7742f4c4d5d490652c3ab8b1875635dbd7
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:35:21 +0530

    Add Good times Good times [B] from Codeforces

---
+++ 2241/B - Good times Good times/solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			String str=String.valueOf(n);
+			int h=str.length();
+			System.out.println((long)Math.pow(10,h)+1);
+		}
+	}
+}
+ 
+ 
