baee2a30aac85ef2d30030a312c3a84a9d70ebba
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:22:17 +0530

    Add Unconventional Pairs [B] from Codeforces

---
+++ 2149/B - Unconventional Pairs/solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a + b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			int min=Integer.MIN_VALUE;
+			int a[]=new int[n];
+			for(int i=0;i<n;i++) a[i]=sc.nextInt();
+			Arrays.sort(a);
+			for(int i=0;i<n-1;i+=2)
+			{
+			    min=Math.max(a[i+1]-a[i],min);
+			}
+			System.out.println(min);
+		}
+	}
+}
+ 
+ 
