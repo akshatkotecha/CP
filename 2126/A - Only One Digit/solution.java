5c1d68d5c41862bc85aef803b7fb1474b1a988d1
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:21:11 +0530

    Add Only One Digit [A] from Codeforces

---
+++ 2126/A - Only One Digit/solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a + b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			int mn=9;
+			while(n > 0) {
+            mn = Math.min(mn, n % 10);
+            n/=10;
+            }
+			System.out.println(mn);
+		}
+	}
+}
+ 
+ 
