bff8ac3a63d77c8cf07a60124ac6419a5cc14521
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:21:43 +0530

    Add Minimize! [A] from Codeforces

---
+++ 2009/A - Minimize!/solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a + b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			System.out.println(b-a);
+		}
+	}
+}
+ 
+ 
