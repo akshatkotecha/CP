63cb88385a09e865ac34f5f93f1dbc427d628ea5
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:25:04 +0530

    Add Mocha and Math [A] from Codeforces

---
+++ 1559/A - Mocha and Math/solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			int a[]=new int[n];
+			for(int i=0;i<a.length;i++) a[i]=sc.nextInt();
+			int ans=a[0];
+			for(int i=1;i<n;i++) ans&=a[i];
+			System.out.println(ans);
+		}
+	}
+}
+ 
+ 
