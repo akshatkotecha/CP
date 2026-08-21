ffd5852d27f920eab2b98c74262be99ee58c5f0a
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:12:02 +0530

    Add Dice Roll Sequence [C] from Codeforces

---
+++ 2195/C - Dice Roll Sequence/solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner sc=new Scanner(System.in);
+	   int t=sc.nextInt();
+	   while(t-->0)
+	   {
+	       int n=sc.nextInt();
+	      // int k=sc.nextInt();
+	       int a[]=new int[n];
+	       for(int i=0;i<n ;i++)  a[i]=sc.nextInt();
+	       int ans=0;
+	       for(int i=1;i<n;i++)
+	       {
+	           if(a[i]==a[i-1] || a[i-1]+a[i]==7)
+	           {
+	               ans++;
+	               a[i]=0;
+	           }
+	       }
+	       System.out.println(ans);
+	   }
+ 
+	}
+}
