50dc507d397acce5b18f528aab0be727c43ffb27
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:17:03 +0530

    Add Trippi Troppi [A] from Codeforces

---
+++ 2094/A - Trippi Troppi/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ 
+public class Codechef {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            String a=sc.next();
+            String b=sc.next();
+            String c=sc.next();
+            System.out.println("" + a.charAt(0) + b.charAt(0) + c.charAt(0));
+        }
+    }
+}
