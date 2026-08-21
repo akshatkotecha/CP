d1740d6b3a5bbe826e071769f3af09908e488d7e
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:17:07 +0530

    Add Shortest Increasing Path [A] from Codeforces

---
+++ 2147/A - Shortest Increasing Path/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ 
+public class Codechef {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            if(x==y || x==y+1 || y==1) System.out.println(-1);
+            else if(x>y) System.out.println(3);
+            else System.out.println(2);
+           
+        }
+    }
+}
