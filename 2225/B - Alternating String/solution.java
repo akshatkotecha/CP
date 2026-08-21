579933845345a6b38ed9112f4c7b416f242e9532
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:22:06 +0530

    Add Alternating String [B] from Codeforces

---
+++ 2225/B - Alternating String/solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a + b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			String s=sc.next();
+			int c=0;
+			for(int i=0;i<s.length()-1;i++)
+			{
+			    if(s.charAt(i)==s.charAt(i+1)) c++;
+			}
+			if(c<=2) System.out.println("YES");
+			else System.out.println("NO");
+		}
+	}
+}
+ 
+ 
