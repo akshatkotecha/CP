2b689bc91437123e7dad902c8bab36e11d4a58cb
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:36:28 +0530

    Add farmpiggie and Subset Sum [A] from Codeforces

---
+++ 2246/A - farmpiggie and Subset Sum/solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			for(int i=n;i>=1;i--){
+			    System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+ 
+ 
