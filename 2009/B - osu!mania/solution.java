19c2b4457ec7f26bbb417d27be12746f005cba9b
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:21:50 +0530

    Add osu!mania [B] from Codeforces

---
+++ 2009/B - osu!mania/solution.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a + b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			int c=0;
+			int b[]=new int[n];
+			for(int i=0;i<n;i++)
+			{
+			    String s=sc.next();
+			    char a[]=s.toCharArray();
+			    for(int j=0;j<4;j++)
+			    {
+			        // a[j]=sc.nextChar();
+			        if(a[j]=='#') b[c++]=j+1;
+			    }
+			}
+			for(int i=n-1;i>=0;i--) System.out.print(b[i]+" ");
+			System.out.println();
+		}
+	}
+}
+ 
+ 
+ 
