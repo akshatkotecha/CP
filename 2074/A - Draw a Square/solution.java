b1e3cdf96c47422d16da4a704eefaa420a4512e9
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:23:40 +0530

    Add Draw a Square [A] from Codeforces

---
+++ 2074/A - Draw a Square/solution.java
@@ -0,0 +1,29 @@
+//2074A
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		//	int n=sc.nextInt();
+			int a[]=new int[4];
+			for(int i=0;i<4;i++) a[i]=sc.nextInt();
+			Arrays.sort(a);
+			if(a[0]==a[3]) System.out.println("Yes");
+			else System.out.println("No");
+		}
+	}
+}
