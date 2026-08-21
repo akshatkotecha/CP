c01886d573694f78634eff283d00786d7f0b94af
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:26:32 +0530

    Add AB Balance [A] from Codeforces

---
+++ 1606/A - AB Balance/solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		//	int n=sc.nextInt();
+		//	int a[]=new int[n];
+		//	for(int i=0;i<n;i++) a[i]=sc.nextInt();
+		    String s=sc.next();
+		    char a[]=s.toCharArray();
+		    int n=a.length;
+		    a[0]=a[n-1];
+		    String s1=new String(a);
+		    System.out.println(s1);
+		}
+	}
+}
+ 
+ 
