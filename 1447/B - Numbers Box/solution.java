4826a1ba5b2c70b0c76f52f4bb071b76871fdd8a
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:39:12 +0530

    Add Numbers Box [B] from Codeforces

---
+++ 1447/B - Numbers Box/solution.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+        public static boolean isPrime(int n) {
+		if (n < 2) return false;
+		if (n < 4) return true;
+		if (n % 2 == 0 || n % 3 == 0) return false;
+		for (int i = 5; i * i <= n; i += 6){
+			if (n % i == 0 || n % (i + 2) == 0)   return false;
+        }
+		return true;
+		}
+	public static boolean[] sievePrime(int n) {
+		boolean[] p = new boolean[n + 1];
+		Arrays.fill(p, true);
+		if (n >= 0) p[0] = false;
+		if (n >= 1) p[1] = false;
+		for (int i = 2; (int) i * i <= n; i++)
+			if (p[i])
+				for (int j = i * i; j <= n; j += i) p[j] = false;
+		return p;
+		}
+ 
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+            int m=sc.nextInt();
+			int a[][]= new int [n][m];
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    a[i][j]=sc.nextInt();
+                }
+            }
+            int neg=0;
+            int min=Integer.MAX_VALUE;
+            int sum=0;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(a[i][j]<0){
+                        neg++;
+                    }
+                    min=Math.min(min,Math.abs(a[i][j]));
+                    sum+=Math.abs(a[i][j]);
+                }
+            }
+            if(neg%2==0) System.out.println(sum);
+            else System.out.println(sum-2*Math.abs(min));
+            
+		}
+	}
+}
+ 
+ 
