ce84e89cb5d9f02c224266fcccb1f145502f39fa
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:21:15 +0530

    Add Blocked [A] from Codeforces

---
+++ 2220/A - Blocked/solution.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a + b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			int a[]=new int[n];
+			HashSet<Integer> set=new HashSet<>();
+			for(int i=0;i<n;i++) 
+			{
+			    a[i]=sc.nextInt();
+			    set.add(a[i]);
+			}
+			Arrays.sort(a);
+			if(set.size()==n)
+			{
+			    for(int i=n-1;i>=0;i--)
+			    {
+			        System.out.print(a[i]+" ");
+			        
+			    }
+			    System.out.println();
+			}
+			else System.out.println(-1);
+		}
+	}
+}
+ 
+ 
