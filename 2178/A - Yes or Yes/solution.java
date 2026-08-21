f55d5a7e06fc4f5627b97d40d72d60a6182c07a4
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:09:26 +0530

    Add Yes or Yes [A] from Codeforces

---
+++ 2178/A - Yes or Yes/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ 
+public class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0) {
+            String s = sc.next();
+            int y = 0;
+            for (int i = 0; i < s.length(); i++)   if (s.charAt(i) == 'Y') y++;
+            System.out.println(y <= 1 ? "YES" : "NO");
+        }
+    }
+}
