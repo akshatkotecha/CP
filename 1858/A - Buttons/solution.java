ef3c6ce0eaec3c0ccf0a3a1447af04f6b9e24234
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:30:50 +0530

    Add Buttons [A] from Codeforces

---
+++ 1858/A - Buttons/solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			int c=sc.nextInt();
+			if(c%2==1) {
+			    if(b<=a) System.out.println("First");
+			    else System.out.println("Second");
+			}
+			else
+			{
+			    if(a>b) System.out.println("First");
+			    else System.out.println("Second");
+			}
+			
+		}
+	}
+}
+ 
+ 
