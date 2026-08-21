f93148bac10aa5a81a4d64ac4b6517c249cfc61b
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:32:31 +0530

    Add Longest Divisors Interval [B] from Codeforces

---
+++ 1855/B - Longest Divisors Interval/solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			long n=sc.nextLong();
+			for(int i=1;i<=500000;i++){
+			    if(n%i!=0){
+			        System.out.println(i-1);
+			        break;
+			    }
+			}
+		}
+	}
+}
+ 
+ 
