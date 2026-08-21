9def2bc14bf9af2bb29bfd2175190983c1bf0753
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:28:04 +0530

    Add Construct an Array [A] from Codeforces

---
+++ 2231/A - Construct an Array/solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			for(int i=n+1;i<=2*n;i++) System.out.print(i+" ");
+			System.out.println();
+		}
+	}
+}
+ 
+ 
