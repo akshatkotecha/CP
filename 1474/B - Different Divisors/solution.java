4e671334f0b7aaf2acc227ea9327e092a55a32de
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:39:17 +0530

    Add Different Divisors [B] from Codeforces

---
+++ 1474/B - Different Divisors/solution.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+        public static boolean isPrime(int n) {
+		if (n < 2) return false;
+		if (n < 4) return true;
+		if (n % 2 == 0 || n % 3 == 0) return false;
+		for (int i = 5; i * i <= n; i += 6){
+			if (n % i == 0 || n % (i + 2) == 0)   return false;
+        }
+		return true;
+		}
+	public static boolean[] sievePrime(int n) {
+		boolean[] p = new boolean[n + 1];
+		Arrays.fill(p, true);
+		if (n >= 0) p[0] = false;
+		if (n >= 1) p[1] = false;
+		for (int i = 2; (int) i * i <= n; i++)
+			if (p[i])
+				for (int j = i * i; j <= n; j += i) p[j] = false;
+		return p;
+		}
+ 
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+        boolean[] isPrime = sievePrime(100000);
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+            int p=0;
+            int q=0;
+            for(int i=2;i<1000000;i++){
+                if(isPrime[i] && i>=1+n){
+                    p=i;
+                    break;
+                }
+            }
+            for(int i=2;i<1000000;i++){
+                if(isPrime[i] && i>=p+n){
+                    q=i;
+                    break;
+                }
+            }
+            System.out.println(Math.min((int)Math.pow(p,3),p*q));
+		}
+	}
+}
+ 
+ 
