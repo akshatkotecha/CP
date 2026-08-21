1acb903b2bc609667aa765a80176c16e8edcaaae
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:35:05 +0530

    Add Crimson Triples [B] from Codeforces

---
+++ 2238/B - Crimson Triples/solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			long ans=0;
+			for(int i=1;i<=n;i++) ans+=1L*(n/i)*(n/i);
+			System.out.println(ans);
+	        
+	        
+		}
+	}
+}
+ 
+ 
