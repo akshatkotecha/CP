7e90ddce18046aa12a30227011f5442651882fc3
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:09:39 +0530

    Add New Year String [A] from Codeforces

---
+++ 2182/A - New Year String/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ 
+public class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+            if (s.contains("2026") || !s.contains("2025")) System.out.println(0);
+            else   System.out.println(1);
+        }
+    }
+}
