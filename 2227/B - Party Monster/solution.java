7f36f101c229613766f3d51374d0fb96bcf53ea2
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:24:01 +0530

    Add Party Monster [B] from Codeforces

---
+++ 2227/B - Party Monster/solution.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+		//	int a[]=new int[n];
+		//	for(int i=0;i<n;i++) a[i]=sc.nextInt();
+		    String s=sc.next();
+		    int c=0;
+		    for(int i=0;i<n;i++)
+		    {
+		        if(s.charAt(i)=='(') c++;
+		        else c--;
+		    }
+		    if(c!=0) System.out.println("NO");
+		    else System.out.println("YES");
+		}
+	}
+}
+ 
+ 
