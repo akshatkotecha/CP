0a5e41598933c8d0d16e2eccfb5d54712fb86a4e
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-21 23:35:17 +0530

    Add RemovevomeR [C] from Codeforces

---
+++ 2241/C - RemovevomeR/solution.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			String s=sc.next();
+			int c=1;
+			if(n==1) System.out.println(1);
+			else{
+			for(int i=1;i<n;i++){
+			    if(s.charAt(i)!=s.charAt(i-1))  c++;
+			}
+			if(c==2) System.out.println(2);
+			else System.out.println(1);
+			}
+			
+		}
+	}
+}
+ 
+ 
