ea0af1d1d6b6e1077ef64ee75cde385a2795d158
Akshat Kotecha <akshatkotecha@gmail.com>
2026-08-23 12:04:29 +0530

    Add Array merging [B] from Codeforces

---
+++ 1831/B - Array merging/solution.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	//long msb = (long) (Math.log(n) / Math.log(2));
+	public static int gcd(int a, int b) {
+        	if (b == 0) return a;
+        	return gcd(b, a % b);
+    		}
+	public static int lcm(int a, int b) {
+    		return (a * b) / gcd(a, b);
+		}
+        public static boolean isPrime(int n) {
+		if (n < 2) return false;
+		if (n < 4) return true;
+		if (n % 2 == 0 || n % 3 == 0) return false;
+		for (int i = 5; i * i <= n; i += 6){
+			if (n % i == 0 || n % (i + 2) == 0)   return false;
+        }
+		return true;
+		}
+	public static boolean[] sievePrime(int n) {
+		boolean[] p = new boolean[n + 1];
+		Arrays.fill(p, true);
+		if (n >= 0) p[0] = false;
+		if (n >= 1) p[1] = false;
+		for (int i = 2; (int) i * i <= n; i++)
+			if (p[i])
+				for (int j = i * i; j <= n; j += i) p[j] = false;
+		return p;
+		}
+ 
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		//   boolean[] isPrime = sievePrime(100000);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+            int a[]=new int[n];
+            int b[]=new int[n];
+            for(int i=0;i<n;i++) a[i]=sc.nextInt();
+            for(int i=0;i<n;i++) b[i]=sc.nextInt();
+            int c[]=new int[2*n+1];
+            int d[]=new int[2*n+1];
+            int count=1;
+            for(int i=1;i<n;i++){
+                if(a[i]==a[i-1]) count++;
+                else{
+                    c[a[i-1]]=Math.max(c[a[i-1]],count);
+                    count=1;
+                }
+            }
+            c[a[n-1]]=Math.max(c[a[n-1]],count);
+            count=1;
+            for(int i=1;i<n;i++){
+                if(b[i]==b[i-1]) count++;
+                else{
+                    d[b[i-1]]=Math.max(d[b[i-1]],count);
+                    count=1;
+                }
+            }
+            d[b[n-1]]=Math.max(d[b[n-1]],count);
+            int  max=-1;
+            for(int i=0;i<c.length;i++){
+                max=Math.max(max,c[i]+d[i]);
+            }
+            System.out.println(max);
+		}
+	}
+}
